45021ac11016fefb28afdaa860738b637165511f
Antoine Willerval <wilate@yahoo.fr>
2020-03-11 17:18:34 +0100

    feat: store Chunk location; fix: bad chunk formation

--- src/main/java/ssixprojet/common/world/Chunk.java
+++ src/main/java/ssixprojet/common/world/Chunk.java
@@ -8,7 +8,13 @@ import ssixprojet.common.entity.Entity;
 
 @Getter
 public class Chunk {
+	private double unit, x, y;
+	Chunk top, bottom, left, right;
 	private List<Entity> entityList = new ArrayList<>();
 
-	public Chunk() {}
+	public Chunk(double unit, double x, double y) {
+		this.unit = unit;
+		this.x = x;
+		this.y = y;
+	}
 }
--- src/main/java/ssixprojet/common/world/World.java
+++ src/main/java/ssixprojet/common/world/World.java
@@ -11,14 +11,28 @@ import ssixprojet.server.AtlasGame;
 public class World {
 	private final List<Entity> entities = new ArrayList<>();
 	private final List<Spawn> spawns = new ArrayList<>();
-	private final Chunk[][] chunks = new Chunk[AtlasGame.getConfig().getChunkSplit()][AtlasGame.getConfig()
-			.getChunkSplit()];
+	private final int split = AtlasGame.getConfig().getChunkSplit();
+	private final Chunk[][] chunks = new Chunk[split][split];
 
 	public World() {
+		double unit, x = 0, y = 0;
+		unit = 1. / split;
 		int i, j;
-		for (i = 0; i < chunks.length; i++)
-			for (j = 0; j < chunks[i].length; i++)
-				chunks[i][j] = new Chunk();
+		for (i = 0; i < chunks.length; i++, y += unit)
+			for (j = 0; j < chunks[i].length; j++, x += unit)
+				chunks[i][j] = new Chunk(unit, x, y);
+
+		for (i = 0; i < chunks.length - 1; i++)
+			for (j = 0; j < chunks[i].length - 1; j++) {
+				chunks[i][j].bottom = chunks[i][j + 1];
+				chunks[i][j].right = chunks[i + 1][j];
+			}
+
+		for (i = 1; i < chunks.length; i++)
+			for (j = 1; j < chunks[i].length; j++) {
+				chunks[i][j].top = chunks[i][j - 1];
+				chunks[i][j].left = chunks[i - 1][j];
+			}
 	}
 
 	/**
